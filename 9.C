36a61a86bc94102cac2326b19b47f2e5b619cb59
Vishnu Bhagwat <vishnu.bhagwat007@gmail.com>
2020-02-13 00:28:31 +0530

    Update and rename PROG20.C to 9.C

--- 9.C
+++ 9.C
@@ -1,11 +1,13 @@
+//To find the fibonacci series upto a given input number...
+
 #include<stdio.h>
 #include<conio.h>
 void main()
 {
  int a=0,b=1 ,c,i,n ;
  clrscr();
- printf("Name: Tushar Malhotra\n");
- printf("Roll Number: 42513202718\n\n") ;
+ printf("Name: Vishnu Bhagwat\n");
+ printf("Roll Number: 41513202718\n\n") ;
  printf("Enter any number:\t");
  scanf("%d",&n) ;
  printf("\nFibonnaci series: %d\t",a) ;
