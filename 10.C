2b8d01eeb28be6ef89e3f45bb19109229a90b604
Vishnu Bhagwat <vishnu.bhagwat007@gmail.com>
2020-02-13 00:30:03 +0530

    Update and rename PROG19.C to 10.C

--- 10.C
+++ 10.C
@@ -1,11 +1,13 @@
+//To find whether a given input number is a prime number or not...
+
 #include<stdio.h>
 #include<conio.h>
 void main()
 {
  int i,n ;
  clrscr();
- printf("Name: Tushar Malhotra\n");
- printf("Roll Number: 42513202718\n\n") ;
+ printf("Name: Vishnu Bhagwat\n");
+ printf("Roll Number: 43513202718\n\n") ;
  printf("Enter any number:\t");
  scanf("%d",&n) ;
  for(i=2 ; i<n ; i++)
