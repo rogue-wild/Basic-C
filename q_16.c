7f5f0430d708e0c5f01a6cd71927e39a53fd145b
Vishnu Bhagwat <vishnu.bhagwat007@gmail.com>
2020-02-12 11:15:37 +0530

    Update q_16.c

--- q_16.c
+++ q_16.c
@@ -4,7 +4,7 @@ void main()
 {
     char ch;
     int d;
-    printf("Vikas Bishnoi \n40913202718");
+    printf("Vishnu Bhagwat \n41913202718");
     printf("\nEnter a character\n");
     scanf("%c",&ch);
     printf("The ASCII value of %c is %d",ch,ch);
