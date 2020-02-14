e00eea29fef28e07cdadb5f2fe1c8054ac9a0893
Vishnu Bhagwat <vishnu.bhagwat007@gmail.com>
2020-02-14 22:49:38 +0530

    Update and rename PROG17.C to 12.C

--- 12.C
+++ 12.C
@@ -1,11 +1,13 @@
+//To find the Factorial of given input Number...
+
 #include<stdio.h>
 #include<conio.h>
 void main()
 {
  int fact=1,i,n ;
  clrscr();
- printf("Name: Tushar Malhotra\n");
- printf("Roll Number: 42513202718\n\n") ;
+ printf("Name: Vishnu Bhagwat\n");
+ printf("Roll Number: 41513202718\n\n") ;
  printf("Enter any number:\t");
  scanf("%d",&n) ;
  for(i=1 ; i<=n ; i++)
