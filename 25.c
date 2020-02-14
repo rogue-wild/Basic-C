f5986ec738dc2d5bc4746b95edeb4c52bd526e1b
Vishnu Bhagwat <vishnu.bhagwat007@gmail.com>
2020-02-14 22:59:27 +0530

    Update 25.c

--- 25.c
+++ 25.c
@@ -1,3 +1,9 @@
+/* To print a pattern 5 4 3 2 1
+                        4 3 2 1
+                          3 2 1
+                            2 1
+                              1   */
+
 #include<stdio.h>
 #include<conio.h>
 
